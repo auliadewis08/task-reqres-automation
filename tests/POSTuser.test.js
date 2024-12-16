2bd1102c2219b23ac0464c3ea9b60f582ce46a77
Aulia Dewi <auliadewis08@gmail.com>
2024-12-16 10:39:38 +0700

    Initial commit

--- tests/POSTuser.test.js
+++ tests/POSTuser.test.js
@@ -3,12 +3,12 @@ const { test, expect } = require('@playwright/test');
 test('POST /api/users', async ({ request }) => {
   const response = await request.post('https://reqres.in/api/users', {
     data: {
-      name: 'John Doe',
-      job: 'Software Engineer'
+      name: 'Aulia Dewi',
+      job: 'Quality Assurance'
     }
   });
   expect(response.status()).toBe(201);
   const responseBody = await response.json();
-  expect(responseBody.name).toBe('John Doe');
-  expect(responseBody.job).toBe('Software Engineer');
+  expect(responseBody.name).toBe('Aulia Dewi');
+  expect(responseBody.job).toBe('Quality Assurance');
 });
--- tests/PUTuser.test.js
+++ tests/PUTuser.test.js
@@ -3,12 +3,12 @@ const { test, expect } = require('@playwright/test');
 test('PUT /api/users/{id}', async ({ request }) => {
   const response = await request.put('https://reqres.in/api/users/2', {
     data: {
-      name: 'Jane Doe',
-      job: 'Lead Engineer'
+      name: 'Aulia Dewi',
+      job: 'QA Analyst'
     }
   });
   expect(response.status()).toBe(200);
   const responseBody = await response.json();
-  expect(responseBody.name).toBe('Jane Doe');
-  expect(responseBody.job).toBe('Lead Engineer');
+  expect(responseBody.name).toBe('Aulia Dewi');
+  expect(responseBody.job).toBe('QA Analyst');
 });
